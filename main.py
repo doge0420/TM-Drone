606572e2a59c7071c7d9b9cad1994f74582a764d
Doge0420 <ethansoya@gmail.com>
2022-03-23 10:20:36 +0100

    ouir

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from time import sleep
-from threading import Thread
-import sys
-import drone
+import colordetection
 
 if __name__ == '__main__':
-    window = Thread(target=drone.capture)
-    window.start()
-
-    tello.takeoff()
-    tello.move_up(20)
-
-    window.join()
+    colordetection
